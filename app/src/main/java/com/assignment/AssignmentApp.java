f41deb8918c3ca32fed8ae6eb6af193163a1a4e3
Mahesh <devmaheshrb458@gmail.com>
2018-04-13 09:43:47 +0530

    Application initial setup

---
+++ app/src/main/java/com/assignment/AssignmentApp.java
@@ -0,0 +1,19 @@
+package com.assignment;
+
+import android.app.Application;
+
+/**
+ * This class executed before any application component.
+ *
+ *
+ * @author Mahesh R Bhatkande (mahesh.bhatkande@infosys.com)
+ * @since 13 Apr, 2018
+ */
+
+public class AssignmentApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
